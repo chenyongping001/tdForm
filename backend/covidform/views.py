f22d2c58daceb4d1a9034013a39a8f15cda066ea
chenyongping <chenyongping001@gmail.com>
2021-11-19 17:20:03 +0800

    myTempInto first

--- backend/covidform/views.py
+++ backend/covidform/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework import viewsets
+from datetime import datetime, timedelta
 from .models import TempInto, TempintoFile
 from .serializers import TempIntoSerializer, TempintoFileSerializer
 
@@ -9,6 +10,22 @@ from .serializers import TempIntoSerializer, TempintoFileSerializer
 class TempIntoViewSet(viewsets.ModelViewSet):
     queryset = TempInto.objects.all()
     serializer_class = TempIntoSerializer
+
+    def get_queryset(self):
+        status = self.request.query_params.get('status')
+        days = self.request.query_params.get('days')
+        weixinID = self.request.query_params.get('weixinid')
+        queryset = TempInto.objects.all()
+        if(status):
+            queryset = queryset.filter(status=status)
+        if(days and days.isnumeric()):
+            end_date = datetime.today()
+            start_date = end_date - timedelta(days=int(days))
+            queryset = queryset.filter(
+                createtime__range=[start_date, end_date])
+        if(weixinID):
+            queryset = queryset.filter(weixinID=weixinID)
+        return queryset.order_by("-createtime")
 
 
 class TempintoFileViewSet(viewsets.ModelViewSet):
--- frontend/pages/myTempInto/myTempInto.js
+++ frontend/pages/myTempInto/myTempInto.js
@@ -5,12 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    formTabTextCol:"black",
-    userTabTextCol:"darkseagreen",
-    
+    formTabTextCol: "black",
+    userTabTextCol: "darkseagreen",
+    tempintoList: [],
+    session: getApp().globalData.sessionId,
+    status: ['待处理', '找不到对应联系人', '已生成申请单', '审批中', '通过', '拒绝', '已删除'],
   },
-  userTabSelect(e){},
-  formTabSelect(e){
+  userTabSelect(e) {},
+  formTabSelect(e) {
     wx.redirectTo({
       url: '/pages/tempIntoAdd/tempIntoAdd',
     })
@@ -20,9 +22,41 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
+    var list = wx.getStorageSync("TEMPINTOLIST")
+    if (list) { // 本地如果有缓存列表，提前渲染
+      that.setData({
+        tempintoList: list
+      })
+    }
+    if (!that.data.session) {
+      wx.redirectTo({
+        url: '/pages/tempIntoAdd/tempIntoAdd',
+      })
+      return
+    }
+    wx.request({
+      url: 'https://www.tzpp.org/tdform/covidform/tempintos/?weixinid=' + that.data.session,
+      success: function (res) {
+        if (res.statusCode === 200) {
+          let list = res.data
+          that.setData({ // 再次渲染列表
+            tempintoList: list
+          })
+          wx.setStorageSync("TEMPINTOLIST", list) // 覆盖缓存数据
+
+        }
+
+      }
+
+    })
 
   },
 
+  onDelete:function(e){
+    console.log(e)
+    let id = e.currentTarget.dataset.id
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -41,7 +75,7 @@ Page({
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
